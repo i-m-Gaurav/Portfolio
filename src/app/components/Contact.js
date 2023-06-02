e5a351dae6442df16d17fc7c6fdc6ddbab1916bf
unknown <indiagauravkumar@gmail.com>
2023-06-02 11:01:27 +0530

    added the socials link

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 import Image from "next/image";
+import Link from "next/link";
 
 const Contact = () => {
   return (
@@ -14,7 +15,6 @@ const Contact = () => {
 
       {/* Cards */}
       <div className="flex justify-center items-center mb-8 w-[80%] md:w-[30%] sm:w-[60%] ">
-
         {/* Card 1 */}
         <div className="bg-[#1F1F1F] rounded">
           {/* image */}
@@ -38,101 +38,98 @@ const Contact = () => {
             <p className="text-[#A7A6A6] px-4 mt-28">Connect with me</p>
             {/* socials */}
             <div className="px-4 mt-2 flex mb-6 ">
-              <FaInstagram className="text-gray-200 text-3xl mr-4" />
-              <FaLinkedin className="text-gray-200 text-3xl mr-4" />
-              <FaGithub className="text-gray-200 text-3xl mr-4" />
-              <FaTwitter className="text-gray-200 text-3xl mr-4" />
+              <Link href="https://www.instagram.com/i.m.gauravkr/">
+                <FaInstagram className="text-gray-200 text-3xl mr-4" />
+              </Link>
+              <Link href="https://www.linkedin.com/in/i-am-gaurav/">
+                <FaLinkedin className="text-gray-200 text-3xl mr-4" />
+              </Link>
+              <Link href="https://github.com/i-m-Gaurav">
+                <FaGithub className="text-gray-200 text-3xl mr-4" />
+              </Link>
+              <Link href="https://twitter.com/__kumargaurav">
+                <FaTwitter className="text-gray-200 text-3xl mr-4" />
+              </Link>
             </div>
           </div>
         </div>
 
-
-     
-
         {/* contact form */}
-        
-      
-
-
-      
-
-
       </div>
 
       {/* form */}
 
       <div className="flex justify-center">
-      <div className="max-w-md w-full text-[#00df9a] p-8 rounded">
-        <form>
-          <div className="mb-6 flex">
-            <div className="mr-2">
-              <label htmlFor="name" className="block font-medium mb-2">
-                Name
+        <div className="max-w-md w-full text-[#00df9a] p-8 rounded">
+          <form>
+            <div className="mb-6 flex">
+              <div className="mr-2">
+                <label htmlFor="name" className="block font-medium mb-2">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full border-gray-300 rounded p-2"
+                  placeholder="Enter your name"
+                />
+              </div>
+              <div>
+                <label htmlFor="phone" className="block font-medium mb-2">
+                  Phone
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  className="w-full border-gray-300 rounded p-2"
+                  placeholder="Enter your phone number"
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block font-medium mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full border-gray-300 rounded p-2"
+                placeholder="Enter your email address"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="subject" className="block font-medium mb-2">
+                Subject
               </label>
               <input
                 type="text"
-                id="name"
+                id="subject"
                 className="w-full border-gray-300 rounded p-2"
-                placeholder="Enter your name"
+                placeholder="Enter the subject"
               />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="message" className="block font-medium mb-2">
+                Message
+              </label>
+              <textarea
+                id="message"
+                className="w-full border-gray-300 rounded p-2"
+                rows="4"
+                placeholder="Enter your message"
+              ></textarea>
             </div>
             <div>
-              <label htmlFor="phone" className="block font-medium mb-2">
-                Phone
-              </label>
-              <input
-                type="tel"
-                id="phone"
-                className="w-full border-gray-300 rounded p-2"
-                placeholder="Enter your phone number"
-              />
+              <button
+                type="submit"
+                className="bg-[#00df9a] text-black font-bold py-2 px-4 rounded  hover:bg-[#00fd9a]"
+              >
+                Send Message
+              </button>
             </div>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block font-medium mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full border-gray-300 rounded p-2"
-              placeholder="Enter your email address"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="subject" className="block font-medium mb-2">
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              className="w-full border-gray-300 rounded p-2"
-              placeholder="Enter the subject"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="block font-medium mb-2">
-              Message
-            </label>
-            <textarea
-              id="message"
-              className="w-full border-gray-300 rounded p-2"
-              rows="4"
-              placeholder="Enter your message"
-            ></textarea>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="bg-[#00df9a] text-black font-bold py-2 px-4 rounded  hover:bg-[#00fd9a]"
-            >
-              Send Message
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-      
     </div>
   );
 };
