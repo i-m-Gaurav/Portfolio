c1cf7f942bcc83e1e01c65f0e33f68af6e0e1ca0
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-06-21 04:45:35 +0530

    chore: Update Cards component to include image prop and adjust styling

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image"
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link"
+
 
 const Cards = ({ title, description, stack, github, live,image }: any) => {
     return (
         <div className="max-w-sm w-full bg-[#76767619] text-xs  border border-slate-600 shadow-lg rounded-lg overflow-hidden mx-auto md:mx-4 mb-4">
-           <div className="p-2 flex justify-between text-xs items-center border-b border-slate-600">
-                <h1 className="text-xs md:text-sm font-semibold text-white">{title}</h1>
-                <a href={github} className="text-blue-500 px-2 hover:underline text-xs md:text-sm">Github link</a>
+           <div className="p-2 flex justify-start text-xs items-center border-b border-slate-600">
+                <h1 className="text-xs mr-4 md:text-sm font-semibold text-white">{title}</h1>
+                <Link href = "www.github.com">
+                <FaGithub size={20}/>
+
+                </Link>
             </div>
             <div className="p-2 md:p-4 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
                 <div className="flex-1">
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -4,30 +4,51 @@ import Cards from "../components/Cards";
 
 const Projects = () => {
     return (
-        <div className="flex flex-wrap justify-center bg-red-500 p-2 max-w-7xl mx-auto mt-24 sm:px-6 lg:px-8">
+        <div className="flex flex-wrap justify-center  p-2 max-w-7xl mx-auto mt-24 sm:px-6 lg:px-8">
             <div className="flex flex-wrap w-full justify-center lg:justify-start">
                 <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
                     <Cards 
                         title="GGV Mart" 
-                        description="A dynamic marketplace app enabling users to buy, sell, and trade items locally with ease. Connect with your community and find great deals effortlessly." 
+                        description="A dynamic marketplace app enabling users to buy, sell, and trade items locally with ease. Connect with your community." 
                         image="/ggvmart.png" 
                     />
                 </div>
                 <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
-                    <Cards title="CosmoDrome"/>
+                    <Cards 
+                        title="CosmoDrome"
+                        description = "A web app showcasing daily images captured by the Mars rover. Explore an extensive gallery of Martian landscapes."
+                        image = "/cosmodrome.png" />
+                        
+                    </div>
+                    <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
+                    <Cards 
+                        title="GGV Mart" 
+                        description="A dynamic marketplace app enabling users to buy, sell, and trade items locally with ease. Connect with your community." 
+                        image="/ggvmart.png" 
+                    />
                 </div>
                 <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
-                    <Cards title="GGV Ma"/>
+                    <Cards 
+                        title="CosmoDrome"
+                        description = "A web app showcasing daily images captured by the Mars rover. Explore an extensive gallery of Martian landscapes."
+                        image = "/cosmodrome.png" />
+                        
+                    </div>
+                    <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
+                    <Cards 
+                        title="GGV Mart" 
+                        description="A dynamic marketplace app enabling users to buy, sell, and trade items locally with ease. Connect with your community." 
+                        image="/ggvmart.png" 
+                    />
                 </div>
                 <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
-                    <Cards title="GGV Ma"/>
-                </div>
-                <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
-                    <Cards title="GGV Ma"/>
-                </div>
-                <div className="p-2 w-full sm:w-1/2 lg:w-1/3">
-                    <Cards title="GGV Ma"/>
-                </div>
+                    <Cards 
+                        title="CosmoDrome"
+                        description = "A web app showcasing daily images captured by the Mars rover. Explore an extensive gallery of Martian landscapes."
+                        image = "/cosmodrome.png" />
+                        
+                    </div>
+                
             </div>
         </div>
     );
