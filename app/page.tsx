5267cebb33a0c1d1348e284336a8ebe78e671e40
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-06-27 04:27:32 +0530

    refactor: Remove unused imports and components from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { DotBackgroundDemo } from "@/components/ui/DotBackgroundDemo";
 import Hero from "./components/Hero";
-import { GridBackgroundDemo } from "@/components/ui/GridBackgroundDemo";
 import { InfiniteMovingCardsDemo } from "@/components/ui/Infinite";
 import { InfiniteMovingCardsDemo2 } from "@/components/ui/Infinite2";
 import Projects from "./components/Projects";
@@ -8,14 +7,11 @@ import Projects from "./components/Projects";
 export default function Home() {
   return (
     <main>
-    
      <Hero/>
      <Projects/>
      <DotBackgroundDemo/>
      <InfiniteMovingCardsDemo/>
      <InfiniteMovingCardsDemo2/>
-     
-           
     </main>
   );
 }
