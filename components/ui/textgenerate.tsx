f97c0a96e64d2a6c5607be6c2dc87316df3990e6
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-06-28 03:57:12 +0530

    feat: Improve scroll behavior in GridBackgroundDemo and update resume link

--- components/ui/textgenerate.tsx
+++ components/ui/textgenerate.tsx
@@ -1,18 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "../ui/text-generate-effect";
 
-const words = `Hi visitors, Hi, I'm Gaurav Kumar, a passionate front-end developer
-originally from the quaint town of Nawada in Bihar, now residing in
-Noida. I hold a B.Sc. in Information Technology from Magadh
-University, Bihar, and a Master's in Computer Applications from Guru
-Ghasidas Central University, Bilaspur, Chhattisgarh. With a strong
-enthusiasm for front-end development, I have honed my skills in the
-MERN stack through various personal and full-stack projects. My
-hands-on experience and dedication to coding drive my continuous
-learning and improvement. I'm currently seeking an entry-level
-position at a leading MNC or a dynamic startup where I can contribute
-and grow.
-`;
+const words = `Hi Vistors, I'm Gaurav, a passionate Front-End developer with a strong enthusiasm for MERN stack development. With a B.Sc IT from Magadh University and a Master's in Computer Applications from Guru Ghasidas Central University, I've honed my skills through various projects. I'm dedicated to continuous learning and improvement, seeking an entry-level position at a leading MNC or dynamic startup.`;
 
 export function TextGenerateEffectDemo() {
   return <TextGenerateEffect words={words} />;
