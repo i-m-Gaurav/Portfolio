87d46e67efec9e0f25d3d7fe238f808188cbc044
Gaurav kumar <indiagauravkumar@gmail.com>
2025-02-10 19:01:08 +0530

    changed the favicon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="flex items-center justify-between max-w-7xl mx-auto mt-24 px-4 sm:px-6 lg:px-8">
-      <GridBackgroundDemo/>
+      {/* <GridBackgroundDemo/> */}
     </div>
   );
 };
