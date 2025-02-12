615b23835bba2ce9eb547c146f92856ed161937b
Gaurav kumar <indiagauravkumar@gmail.com>
2025-02-12 10:09:47 +0530

    Add GitHub project fetching functionality and update UI components

--- components/Github.tsx
+++ components/Github.tsx
@@ -5,8 +5,8 @@ import GithubProjectCard from "./GithubProjectCard";
 export default function Github(){
     return(
         <>
-        <div className="w-full border rounded-lg border-red-500 bg-blue-500">
-            <div className = "border-b-2 border-white">All Github Projects</div>
+        <div className="w-full border rounded-lg border-[#30363d] ">
+            <div className = "border-b-2 border-[#30363d] m-2 p-2">All Github Projects</div>
             {/* this is the repo project card.*/}
             <GithubProjectCard/>
         </div>
